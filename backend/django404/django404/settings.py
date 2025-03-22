d343a4f67b2278ea3aba59fae39f409628f3414d
Nishchay <nishchayranjan878@gmail.com>
2025-03-22 05:32:16 -0600

    c

--- backend/django404/django404/settings.py
+++ backend/django404/django404/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 STATIC_URL = '/static/'
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 
-ALLOWED_HOSTS = ['localhost', '2605:fd00:4:1001:f816:3eff:fe8c:5c2d']
+ALLOWED_HOSTS = ['2605:fd00:4:1001:f816:3eff:fe8c:5c2d']
 
 
 AUTH_USER_MODEL = 'myapp.User'
