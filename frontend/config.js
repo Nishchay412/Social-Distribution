1f428c245872579c3784bb5ff5f2de35c03c2b75
qq815101 <qq815101@gmail.com>
2025-03-26 04:46:21 +0000

    address fix

--- frontend/config.js
+++ frontend/config.js
@@ -9,7 +9,7 @@ if (host.includes("fe8c:5c2d")) {
 } else if (host.includes("fecc:9717")) {
   // For Node 2
   API_BASE_URL = "http://[2605:fd00:4:1001:f816:3eff:fecc:9717]:8000";
-} else if (host.includes("fef6:3793")) {
+} else if (host.includes("fe3e:c88d")) {
   // For Node 3
   API_BASE_URL = "http://[2605:fd00:4:1001:f816:3eff:fe3e:c88d]:8000";
 } 
